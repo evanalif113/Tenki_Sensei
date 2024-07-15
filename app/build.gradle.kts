6394dc8ab8896d3c704e117b556c520c9bc84c67
evanalif113 <evanalifwidhyatma@gmail.com>
2024-07-15 08:05:01 +0700

    major update for learning tab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.gridlayout)
     implementation(libs.firebase.firestore)
+    implementation(libs.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/tenkisen/ui/learning/LearningFragment.java
+++ app/src/main/java/com/example/tenkisen/ui/learning/LearningFragment.java
@@ -1,10 +1,11 @@
 package com.example.tenkisen.ui.learning;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -24,7 +25,50 @@ public class LearningFragment extends Fragment {
         binding = FragmentLearningBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        final Button awan = binding.buttonAwan;
+        final Button presipitasi = binding.buttonPresipitasi;
+        final Button ekstrem = binding.buttonEkstrem;
+        final Button angin = binding.buttonAngin;
+
+        // Setting up the onClickListeners for the buttons
+        awan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LearnAwan.class);
+                startActivity(intent);
+            }
+        });
+
+        presipitasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LearnPresipitasi.class);
+                startActivity(intent);
+            }
+        });
+
+        ekstrem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LearnEkstrem.class);
+                startActivity(intent);
+            }
+        });
+
+        angin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LearnAngin.class);
+                startActivity(intent);
+            }
+        });
+
         return root;
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
