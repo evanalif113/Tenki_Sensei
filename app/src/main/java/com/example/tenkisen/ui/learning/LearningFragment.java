04fe0e260e3bb738569455910d374f442befc36b
evanalif113 <evanalifwidhyatma@gmail.com>
2024-07-19 09:41:14 +0700

    update LearningFragment

--- app/src/main/java/com/example/tenkisen/ui/learning/LearningFragment.java
+++ app/src/main/java/com/example/tenkisen/ui/learning/LearningFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
@@ -17,6 +18,7 @@ public class LearningFragment extends Fragment {
 
     private FragmentLearningBinding binding;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         LearningViewModel learningViewModel =
@@ -25,11 +27,12 @@ public class LearningFragment extends Fragment {
         binding = FragmentLearningBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //Tombol mengarah ke tiap materi
+        // Tombol mengarah ke tiap materi
         final Button awan = binding.buttonAwan;
         final Button presipitasi = binding.buttonPresipitasi;
         final Button ekstrem = binding.buttonEkstrem;
         final Button angin = binding.buttonAngin;
+        final WebView webView = binding.webView;
 
         // Setting up the onClickListeners for the buttons
         awan.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +43,6 @@ public class LearningFragment extends Fragment {
             }
         });
 
-        //Class unruk mengarahkan ke layout
         presipitasi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -65,10 +67,22 @@ public class LearningFragment extends Fragment {
             }
         });
 
+        // Initialize the WebView
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://thingspeak.com/" +
+                "channels/2326256/" +
+                "charts/1?" +
+                "bgcolor=%23ffffff" +
+                "&color=%2300b3b3" +
+                "&dynamic=true" +
+                "&results=60" +
+                "&title=Suhu" +
+                "&type=spline"); // or use webView.loadData() to load HTML content directly
+
         return root;
     }
 
-    //Destroy View
+    // Destroy View
     @Override
     public void onDestroyView() {
         super.onDestroyView();
